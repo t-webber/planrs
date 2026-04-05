0e2f8bbc3850d30730ca16b4ef0df62ba50acf44
Tom Webber <t.c.w.webber@gmail.com>
2026-04-05 10:01:51 +0200

    ref: remove useless boilerplate

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,21 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
 fn main() {
-    del_lib::run()
+    run()
 }
